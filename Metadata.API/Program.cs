77ef1604b6b4a67ed588fa4ceafe686810a543ed
Khoa Le <minhkhoabtr8@gmail.com>
2023-09-24 08:09:00 +0700

    Config Swagger UI

--- Metadata.API/Program.cs
+++ Metadata.API/Program.cs
@@ -1,25 +1,65 @@
+using Metadata.API.Extensions;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Serilog;
+using Serilog.Events;
+using SharedLib.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    if (ctx.HostingEnvironment.IsEnvironment("VPS"))
+    {
+        lc.WriteTo.File(ctx.Configuration["Logging:FilePath"]).MinimumLevel.Is(LogEventLevel.Error);
+    }
+    else
+    {
+        lc.ReadFrom.Configuration(ctx.Configuration);
+    }
+});
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(opt =>
+    {
+        opt.SerializerSettings.ContractResolver = new DefaultContractResolver
+        {
+            NamingStrategy = new CamelCaseNamingStrategy
+            {
+                ProcessDictionaryKeys = true
+            }
+        };
+        opt.SerializerSettings.Converters.Add(new StringEnumConverter());
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddJWTAuthentication(builder.Configuration);
+builder.Services.AddDbContext(builder.Configuration);
 builder.Services.AddSwaggerGen();
+builder.Services.AddServices();
+builder.Services.AddHttpClients();
+builder.Services.AddServiceFilters();
+builder.Services.AddRepositories();
+builder.Services.AddMassTransit(builder.Configuration);
+builder.Services.AddUOW();
+builder.Services.AddAutoMapper();
+builder.Services.AddEvents();
+builder.Services.ConfigureSwagger();
+builder.Services.ConfigureApiOptions();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
-
+app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+app.Services.ApplyPendingMigrations();
 
 app.Run();
