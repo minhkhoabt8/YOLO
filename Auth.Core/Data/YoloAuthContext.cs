d3d00f96c578e623fa928987f27e75ec625cd738
Khoa Le <minhkhoabtr8@gmail.com>
2023-10-01 22:28:00 +0700

    Update YoloAuthContext.cs

--- Auth.Core/Data/YoloAuthContext.cs
+++ Auth.Core/Data/YoloAuthContext.cs
@@ -24,10 +24,6 @@ public partial class YoloAuthContext : DbContext
 
     public virtual DbSet<Role> Roles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database=YOLO_Auth;User Id=khoalnm;Password=admin;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
