cd222f0e7d041292b92eee3b1eecfaa9e98c3874
Khoa Le <minhkhoabtr8@gmail.com>
2023-10-26 00:48:41 +0700

    Add Migration For Updated Database

---
+++ Metadata.API/Migrations/20231025174759_UpdateEntities.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metadata.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "project_status",
+                table: "Projects",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "land_info_type",
+                table: "LandPositionInfos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "land_info_type",
+                table: "LandPositionInfos");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "project_status",
+                table: "Projects",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
