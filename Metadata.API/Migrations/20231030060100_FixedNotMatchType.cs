7b5f42954282e356fd0c2c1da2d12b84297f36c5
Khoa Le <minhkhoabtr8@gmail.com>
2023-10-30 13:01:10 +0700

    Add Migrations

---
+++ Metadata.API/Migrations/20231030060100_FixedNotMatchType.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metadata.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedNotMatchType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "implementation_year",
+                table: "Projects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "implementation_year",
+                table: "Projects",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
