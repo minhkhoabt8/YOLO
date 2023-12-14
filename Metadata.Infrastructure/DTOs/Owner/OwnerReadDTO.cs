089e78b30509da150affe1106ae2b00f1d97149a
m4c1st <59388870+minhkhoabt8@users.noreply.github.com>
2023-12-14 18:39:16 +0700

    Add Is Unit Price Land to query Document

--- Metadata.Infrastructure/DTOs/Owner/OwnerReadDTO.cs
+++ Metadata.Infrastructure/DTOs/Owner/OwnerReadDTO.cs
@@ -69,7 +69,7 @@ namespace Metadata.Infrastructure.DTOs.Owner
         //public  OrganizationType? OrganizationType { get; set; }
         public OrganizationTypeReadDTO? OrganizationType { get; set; }
 
-        public PlanInOwnerReadDTO? PlanCode { get; set; }
+        public PlanInOwnerReadDTO? PLan { get; set; }
 
         //public ProjectReadDTO? Project { get; set; }
         public IEnumerable<SupportReadDTO>? Supports { get; set; }
