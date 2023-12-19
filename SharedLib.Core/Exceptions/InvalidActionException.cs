2c386a66acc7b7cb639e76353666361f1685ae01
m4c1st <59388870+minhkhoabt8@users.noreply.github.com>
2023-12-19 17:22:36 +0700

    Update InvalidActionException.cs

--- SharedLib.Core/Exceptions/InvalidActionException.cs
+++ SharedLib.Core/Exceptions/InvalidActionException.cs
@@ -2,7 +2,7 @@
 
 public class InvalidActionException : HandledException
 {
-    public InvalidActionException(string? message = null) : base(400, message ?? "Không thể thực hiện hành động này")
+    public InvalidActionException(string? message = null) : base(400, message ?? "Không thể thực hiện hành động này.")
     {
     }
 }
